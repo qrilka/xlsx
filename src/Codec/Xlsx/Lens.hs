3040e47a86592da06be640192547d33cb865ad59
Kirill Zaborsky <qrilka@gmail.com>
2015-11-24 00:35:06 +0300

    Added lenses with RC/XY cell coordinates, closes #26

--- src/Codec/Xlsx/Lens.hs
+++ src/Codec/Xlsx/Lens.hs
@@ -3,13 +3,20 @@ module Codec.Xlsx.Lens
     ( ixSheet
     , atSheet
     , ixCell
+    , ixCellRC
+    , ixCellXY
     , atCell
+    , atCellRC
+    , atCellXY
     , cellValueAt
+    , cellValueAtRC
+    , cellValueAtXY
  ) where
 
 import Codec.Xlsx.Types
 import Control.Lens
 import Data.Text
+import Data.Tuple (swap)
 
 ixSheet :: Text -> Traversal' Xlsx Worksheet
 ixSheet s = xlSheets . ix s
@@ -18,10 +25,28 @@ atSheet :: Text -> Lens' Xlsx (Maybe Worksheet)
 atSheet s = xlSheets . at s
 
 ixCell :: (Int, Int) -> Traversal' Worksheet Cell
-ixCell i = wsCells . ix i
+ixCell = ixCellRC
+
+ixCellRC :: (Int, Int) -> Traversal' Worksheet Cell
+ixCellRC i = wsCells . ix i
+
+ixCellXY :: (Int, Int) -> Traversal' Worksheet Cell
+ixCellXY = ixCellRC . swap
 
 atCell :: (Int, Int) -> Lens' Worksheet (Maybe Cell)
-atCell i = wsCells . at i
+atCell = atCellRC
+
+atCellRC :: (Int, Int) -> Lens' Worksheet (Maybe Cell)
+atCellRC i = wsCells . at i
+
+atCellXY :: (Int, Int) -> Lens' Worksheet (Maybe Cell)
+atCellXY = atCellRC . swap
 
 cellValueAt :: (Int, Int) -> Lens' Worksheet (Maybe CellValue)
-cellValueAt i = atCell i . non def . cellValue
+cellValueAt = cellValueAtRC
+
+cellValueAtRC :: (Int, Int) -> Lens' Worksheet (Maybe CellValue)
+cellValueAtRC i = atCell i . non def . cellValue
+
+cellValueAtXY :: (Int, Int) -> Lens' Worksheet (Maybe CellValue)
+cellValueAtXY = cellValueAtRC . swap
