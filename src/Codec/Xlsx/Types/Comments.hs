e68e7ab3f85addb5fa8eeead8b8166dbd8a22e91
Kirill Zaborsky <qrilka@gmail.com>
2016-05-12 00:51:41 +0300

    Added comment for not implemented  subelements

--- src/Codec/Xlsx/Types/Comments.hs
+++ src/Codec/Xlsx/Types/Comments.hs
@@ -19,6 +19,12 @@ import           Codec.Xlsx.Writer.Internal
 
 
 -- | User comment for a cell
+--
+-- TODO: the following child elements:
+-- * guid
+-- * shapeId
+-- * commentPr
+--
 -- Section 18.7.3 "comment (Comment)" (p. 1749)
 data Comment = Comment {
     -- | cell comment text, maybe formatted
