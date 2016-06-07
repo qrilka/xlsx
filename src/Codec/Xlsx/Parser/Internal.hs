8caa2b6a32522f684cb072d351f753f8ff3d6eaa
Kirill Zaborsky <qrilka@gmail.com>
2016-06-08 01:15:48 +0300

    Removed some unnecessary dependencies

--- src/Codec/Xlsx/Parser/Internal.hs
+++ src/Codec/Xlsx/Parser/Internal.hs
@@ -24,7 +24,7 @@ import           Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Read as T
 import           Data.Typeable (Typeable)
-import           Data.XML.Types
+import           Text.XML
 import           Text.XML.Cursor
 
 #if !MIN_VERSION_base(4,8,0)
