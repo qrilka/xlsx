891193e79bedb002064199b2632cfd87fdadb565
Kirill Zaborsky <qrilka@gmail.com>
2016-12-11 00:25:48 +0300

    GHC 7.8 compatibility

--- src/Codec/Xlsx/Types/Common.hs
+++ src/Codec/Xlsx/Types/Common.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Codec.Xlsx.Types.Common
   ( CellRef(..)
@@ -23,6 +24,10 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import Text.XML
 import Text.XML.Cursor
+
+#if !MIN_VERSION_base(4,8,0)
+import           Control.Applicative
+#endif
 
 import Codec.Xlsx.Parser.Internal
 import Codec.Xlsx.Types.RichText
