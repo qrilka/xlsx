868c4672b0fd85e0a32b9af0fb50c26101799f3a
Jappie Klooster <jappieklooster@hotmail.com>
2021-10-20 13:59:47 +0300

    expose cabal, disable profiling by default.

--- default.nix
+++ default.nix
@@ -2,8 +2,9 @@ let
   rev = "07ca3a021f05d6ff46bbd03c418b418abb781279"; # first 21.05 release
   url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
   compiler = "ghc884";
+  isLibraryProfiling = false;
   pkgs = import (builtins.fetchTarball url) {
-    config = {
+    config = if isLibraryProfiling then ({
       packageOverrides = pkgs_super: {
         haskell = pkgs_super.haskell // {
           packages = pkgs_super.haskell.packages // {
@@ -17,8 +18,10 @@ let
           };
         };
       };
-    };
+    }) else {};
   };
 
   hpkgs = pkgs.haskell.packages."${compiler}";
-in hpkgs.callCabal2nix "xlsx" ./. {}
+in pkgs.haskell.lib.overrideCabal (hpkgs.callCabal2nix "xlsx" ./. {}) {
+  libraryToolDepends = [pkgs.cabal-install];
+}
