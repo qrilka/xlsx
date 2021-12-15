095fa1c2d93a0b7539b170362a81e802bdd5f0b9
Jappie Klooster <jappieklooster@hotmail.com>
2021-12-15 11:52:36 +0100

    Clean out docs for memoized module
    I copy pasted this but forget to delete the no longer
    relavant words

--- src/Codec/Xlsx/Parser/Internal/Memoize.hs
+++ src/Codec/Xlsx/Parser/Internal/Memoize.hs
@@ -24,8 +24,6 @@ import Control.Exception
 --
 -- Note that this type provides a 'Show' instance for convenience, but not
 -- useful information can be provided.
---
--- @since 0.2.8.0
 newtype Memoized a = Memoized (IO a)
   deriving (Functor, A.Applicative, Monad)
 instance Show (Memoized a) where
@@ -33,18 +31,13 @@ instance Show (Memoized a) where
 
 -- | Extract a value from a 'Memoized', running an action if no cached value is
 -- available.
---
--- @since 0.2.8.0
 runMemoized :: MonadIO m => Memoized a -> m a
 runMemoized (Memoized m) = liftIO m
 {-# INLINE runMemoized #-}
 
 -- | Create a new 'Memoized' value using an 'IORef' under the surface. Note that
 -- the action may be run in multiple threads simultaneously, so this may not be
--- thread safe (depending on the underlying action). Consider using
--- 'memoizeMVar'.
---
--- @since 0.2.8.0
+-- thread safe (depending on the underlying action).
 memoizeRef :: IO a -> IO (Memoized a)
 memoizeRef action = do
   ref <- newIORef Nothing
