788c8ec3833830d08dba25bfc59adcd477b80a3d
Benjamin McRae <benjamin@supercede.com>
2025-08-22 16:13:40 +0100

    add call stack to assertion to bubble up where expected

--- test/Diff.hs
+++ test/Diff.hs
@@ -3,11 +3,11 @@ module Diff where
 import           Data.Algorithm.Diff       (Diff (..), getGroupedDiff)
 import           Data.Algorithm.DiffOutput (ppDiff)
 import           Data.Monoid               ((<>))
-import           Test.Tasty.HUnit          (Assertion, assertBool)
+import           Test.Tasty.HUnit          (Assertion, HasCallStack, assertBool)
 import           Text.Groom                (groom)
 
 -- | Like '@=?' but producing a diff on failure.
-(@==?) :: (Eq a, Show a) => a -> a -> Assertion
+(@==?) :: HasCallStack => (Eq a, Show a) => a -> a -> Assertion
 x @==? y =
     assertBool ("Expected:\n" <> groom x <> "\nDifference:\n" <> msg) (x == y)
   where
